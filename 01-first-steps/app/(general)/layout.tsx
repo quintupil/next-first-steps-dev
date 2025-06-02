2a7710732e00e113fa24aa7accc8f3091e9865ce
Carlos García <gacia.quintupil@gmail.com>
2025-06-01 22:00:44 -0400

    18. Barra de navegación

--- 01-first-steps/app/(general)/layout.tsx
+++ 01-first-steps/app/(general)/layout.tsx
@@ -1,14 +1,17 @@
-export default function AboutLayout({
+import { Navbar } from "@/components";
+
+export default function GeneralLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div>
+    <>
+      <Navbar />
       <main className="flex flex-col items-center p-24">
         <span className="text-5xl">Hola NextJS</span>
         {children}        
       </main>
-    </div>
+    </>
   );
 }
